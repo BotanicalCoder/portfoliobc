11d6209b8627966e33bf89443e2a25d36c67a66b
BotanicalCoder <x3mace007@gmail.com>
2021-01-21 15:52:49 +0100

    update portfolio container

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 import FrontendProjects from "./FrontEnd_projects";
@@ -20,25 +20,3 @@ const PortfolioContainer = () => {
 };
 
 export default PortfolioContainer;
-=======
-import React from "react";
-import FrontendProjects from "./FrontEnd_projects";
-import FullStackProjects from "./FullStack_Projects";
-
-const PortfolioContainer = () => {
-  return (
-    <div id="portfolio">
-      
-      <div>
-        <FrontendProjects />
-      </div>
-
-      <div>
-        <FullStackProjects />
-      </div>
-    </div>
-  );
-};
-
-export default PortfolioContainer;
->>>>>>> 2836a30fa16c1a883ccfb48b4351460764aace2d

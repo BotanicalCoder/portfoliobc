8317635dda8510d0b931b38efb6c0ce746d3b61e
BotanicalCoder <x3mace007@gmail.com>
2021-01-21 15:45:26 +0100

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -50,56 +50,7 @@ const Abtsection = styled.section`
 `;
 
 export default About;
-=======
-import React from "react";
-import styled from "styled-components";
 
-import Title from "./Title";
 
-const About = () => {
-  return (
-    <Abtsection id='about'>
-      <Title title="ABOUT ME"></Title>
-      
-      <h3>
-        MY NAMES ARE :
-        <br />
-        EVANS CHIBUEZE OKOCHA
-      </h3>
-      <p>
-        I am a full stack developer proficient in the use of HTML, CSS,
-        JAVASCRIPT, BOOTSTRAP, EXPRESS, REACT JS and NODE JS. 
-        <br />
-      </p>
-      <p>
-        With knowledge of Angular and Graphql .
-      </p>
-        <p>
-        To build fast responsive and scalable web app from the frontend to the
-        backend
-      </p>
-        <p>
-        <strong>Ready </strong> and <strong>willing</strong> to <em>learn </em>
-        new technologies to build solutions that would make the world a better.
-      </p>
-     
-    </Abtsection>
-  );
-};
 
-const Abtsection = styled.section`
-  background-color: #ffbaba;
-  margin: 0em -2.8em;
-  padding: 1em 5em;
-  font-size: larger;
-  
-  h1 {
-    margin-bottom: 1em;
-  }
-  span {
-    margin: 0em 0em 2em 0em;
-  }
-`;
-
-export default About;
 
